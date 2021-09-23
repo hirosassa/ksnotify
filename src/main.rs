4f74bb3073c08fabc8f5f15480d52923f3bcaea2
hirosassa <hiro.sassa@gmail.com>
2021-09-23 23:26:50 +0900

    file division

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,53 @@
+mod notifier;
+
 use anyhow::Result;
-use gitlab::api::{self, projects::merge_requests::notes::CreateMergeRequestNote, Query};
-use gitlab::Gitlab;
 use handlebars::Handlebars;
+use notifier::Notifiable;
 use serde::Serialize;
-use serde_json;
 use std::fs::File;
 use std::io::{self, Read};
+use std::str::FromStr;
 use std::{env, process};
-use yaml_rust::YamlLoader;
+use strum_macros::EnumString;
+use yaml_rust::{Yaml, YamlLoader};
 
-#[derive(Debug)]
-struct GitlabConfig {
-    base_url: String,
-    token: String,
-    repository: Repository,
+#[derive(Debug, PartialEq, Clone, Copy, EnumString)]
+pub enum CIKind {
+    #[strum(serialize = "gitlab")]
+    GitLab,
 }
 
 #[derive(Debug)]
-struct Repository {
-    owner: String,
-    project: String,
-}
-
 struct MergeRequest {
     number: u64,
     revision: String,
 }
 
-struct CI {
+#[derive(Debug)]
+pub struct CI {
     url: String,
     merge_request: MergeRequest,
 }
 
 impl CI {
-    fn new() -> Result<CI> {
-        let url = env::var("CI_JOB_URL")?;
-        let number = env::var("CI_MERGE_REQUEST_IID")?.parse()?;
-        let revision = env::var("CI_COMMIT_SHA")?;
-        let merge_request = MergeRequest { number, revision };
-        Ok(CI { url, merge_request })
+    fn new(ci: CIKind) -> Result<Self> {
+        match ci {
+            CIKind::GitLab => {
+                // todo: make this as function
+                let url = env::var("CI_JOB_URL")?;
+                let number = env::var("CI_MERGE_REQUEST_IID")?.parse()?;
+                let revision = env::var("CI_COMMIT_SHA")?;
+                let merge_request = MergeRequest { number, revision };
+                Ok(Self { url, merge_request })
+            }
+        }
     }
-}
-
-struct Notifier {
-    client: Client,
-    config: Config,
-    ci: CI,
-}
-
-impl Notifier {
-    fn new(ci: CI, path: &str) -> Result<Self> {
-        let config = Config::new(path)?;
-        let gitlab = Gitlab::new(
-            config.gitlab_config.base_url.to_owned(),
-            config.gitlab_config.token.to_owned(),
-        )?;
-        let client = Client { client: gitlab };
-        Ok(Self { client, config, ci })
-    }
-}
-
-trait Notifiable {
-    fn notify(&self, body: String) -> Result<()>;
-}
-
-impl Notifiable for Notifier {
-    fn notify(&self, body: String) -> Result<()> {
-        let project = format!(
-            "{}/{}",
-            self.config.gitlab_config.repository.owner,
-            self.config.gitlab_config.repository.project
-        );
-        let note = CreateMergeRequestNote::builder()
-            .project(project)
-            .merge_request(self.ci.merge_request.number)
-            .body(body)
-            .build()
-            .map_err(anyhow::Error::msg)?;
-        api::ignore(note).query(&self.client.client)?;
-        Ok(())
-    }
-}
-
-struct Client {
-    client: Gitlab,
 }
 
 #[derive(Debug)]
 struct Config {
-    ci: String,
-    gitlab_config: GitlabConfig,
+    ci: CIKind,
+    notifier: Yaml,
 }
 
 impl Config {
@@ -97,24 +55,12 @@ impl Config {
         let mut f = File::open(path)?;
         let mut config_string = String::new();
         f.read_to_string(&mut config_string)?;
-        let docs = YamlLoader::load_from_str(&config_string).unwrap();
+        let doc = &YamlLoader::load_from_str(&config_string)?[0];
+        let ci_kind = CIKind::from_str(doc["ci"].as_str().expect("failed to load the CI type"))?;
 
         Ok(Self {
-            ci: docs[0]["ci"].as_str().unwrap().to_string(),
-            gitlab_config: GitlabConfig {
-                base_url: docs[0]["gitlab"]["base_url"].as_str().unwrap().to_string(),
-                token: docs[0]["gitlab"]["token"].as_str().unwrap().to_string(),
-                repository: Repository {
-                    owner: docs[0]["gitlab"]["repository"]["owner"]
-                        .as_str()
-                        .unwrap()
-                        .to_string(),
-                    project: docs[0]["gitlab"]["repository"]["project"]
-                        .as_str()
-                        .unwrap()
-                        .to_string(),
-                },
-            },
+            ci: ci_kind,
+            notifier: doc["notifier"].clone(),
         })
     }
 }
@@ -166,8 +112,12 @@ fn main() {
 }
 
 fn run() -> Result<()> {
-    let ci = CI::new()?;
-    let notifier = Notifier::new(ci, "ksnotify.yaml")?;
+    let config = Config::new("ksnotify.yaml")?;
+    let ci = CI::new(config.ci)?;
+
+    let notifier = match config.ci {
+        CIKind::GitLab => notifier::gitlab::GitlabNotifier::new(ci, config.notifier),
+    }?;
 
     let mut body = String::new();
     io::stdin().read_to_string(&mut body)?;
